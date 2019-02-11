66f2e1453008db8cf8caf01e83599b46854ab1f3
thomastran430 <ttt51@scarletmail.rutgers.edu>
2019-02-11 16:00:05 -0500

    Add files via upload

---
+++ Banking_Project/cmake-build-debug/CMakeFiles/Banking_Project.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Banking_Project.dir/main.cpp.obj"
+  "Banking_Project.pdb"
+  "Banking_Project.exe"
+  "Banking_Project.exe.manifest"
+  "libBanking_Project.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/Banking_Project.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
